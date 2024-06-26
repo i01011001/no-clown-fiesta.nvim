df1fc1aa3a4f29a8a7b3bfde2a3e443cfc3e2411
Yugen <134605846+i01011001@users.noreply.github.com>
2024-06-26 10:29:09 +0545

    Create notify.lua

---
+++ lua/no-clown-fiesta/groups/notify.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+function M.highlight(palette, opts)
+  return {
+NotifyERRORBorder={ fg=palette.medium_gray },
+NotifyWARNBorder ={ fg=palette.medium_gray },
+NotifyINFOBorder ={ fg=palette.medium_gray },
+NotifyDEBUGBorder={ fg=palette.medium_gray },
+NotifyTRACEBorder={ fg=palette.medium_gray },
+NotifyERRORIcon  ={ fg=palette.red },
+NotifyWARNIcon   ={ fg=palette.red },
+NotifyINFOIcon   ={ fg=palette.green },
+NotifyDEBUGIcon  ={ fg=palette.cyan },
+NotifyTRACEIcon  ={ fg=palette.cyan },
+NotifyERRORTitle ={ fg=palette.red },
+NotifyWARNTitle  ={ fg=palette.orange },
+NotifyINFOTitle  ={ fg=palette.green },
+NotifyDEBUGTitle ={ fg=palette.yellow },
+NotifyTRACETitle ={ fg=palette.cyan },
+  }
+end
+
+return M
