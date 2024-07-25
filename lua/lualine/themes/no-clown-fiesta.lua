8ee55e207208293f60b004a0622db7db602d67f4
Yugen <134605846+i01011001@users.noreply.github.com>
2024-07-25 13:12:31 +0545

    Update no-clown-fiesta.lua
    remove bg for none

--- lua/lualine/themes/no-clown-fiesta.lua
+++ lua/lualine/themes/no-clown-fiesta.lua
@@ -3,21 +3,22 @@
 local colors = {
   bg = "#000000",
   fg = "#D0D0D0",
+  none = "NONE",
 }
 
 return {
   normal = {
-    a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
-    b = { fg = colors.fg, bg = colors.bg },
-    c = { fg = colors.fg, bg = colors.bg },
+    a = { fg = colors.fg, bg = colors.none, gui = "bold" },
+    b = { fg = colors.fg, bg = colors.none },
+    c = { fg = colors.fg, bg = colors.none },
   },
-  insert = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-  visual = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-  command = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
-  replace = { a = { fg = colors.fg, bg = colors.bg, gui = "bold" } },
+  insert = { a = { fg = colors.fg, bg = colors.none, gui = "bold" } },
+  visual = { a = { fg = colors.fg, bg = colors.none, gui = "bold" } },
+  command = { a = { fg = colors.fg, bg = colors.none, gui = "bold" } },
+  replace = { a = { fg = colors.fg, bg = colors.none, gui = "bold" } },
   inactive = {
-    a = { fg = colors.fg, bg = colors.bg },
-    b = { fg = colors.fg, bg = colors.bg },
-    c = { fg = colors.fg, bg = colors.bg },
+    a = { fg = colors.fg, bg = colors.none },
+    b = { fg = colors.fg, bg = colors.none },
+    c = { fg = colors.fg, bg = colors.none },
   },
 }
